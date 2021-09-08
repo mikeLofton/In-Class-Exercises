a33b36635417dad28c708e34bae3f0ea0e96e100
mikeLofton <michaellofton32@gmail.com>
2021-09-08 16:20:49 -0500

    Started project

---
+++ In Class Exercises/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace In_Class_Exercises
+{
+    class Game
+    {
+        public void Run()
+        {
+            int[] testArray = new int[] { 1, 2, 3, 4, 5 };
+            PrintNumbers(testArray, 5);
+        }
+
+        void PrintNumbers(int[] numbers, int size)
+        {
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+        }
+
+        void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ In Class Exercises/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace In_Class_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of Game
+            Game game = new Game();
+
+            //Run the Game
+            game.Run();
+        }
+    }
+}
